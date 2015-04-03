276e0221fcc51c61bd3d869b743145b635aec4c2
Zackneifein <zackneifein@gmail.com>
2015-04-03 04:37:10 +0200

    Ajout Client + livreur + maps

--- LivraisonRepas/Vues/Client.xaml.cs
+++ LivraisonRepas/Vues/Client.xaml.cs
@@ -4,42 +4,67 @@ using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Navigation;
 using LivraisonRepas.Models;
+using LivraisonRepas.Webservices;
+using System.Collections.Generic;
 
 namespace LivraisonRepas.Vues
 {
     public sealed partial class Client
     {
         private Utilisateurs _userConnected;
+        private Services _service;
+        private List<Restaurants> ListRestaurants;
+        private List<Menus> ListMenus;
+        private MenuRestaurant _menuResto;
+        private Menus _menuSelected;
+        private string RestaurantEnCours;
+        private string MenusEnCours;
 
         public Client()
         {
+            _service = new Services();
+            InitalizeCombobox();
             InitializeComponent();
         }
 
-        private void AjouterPanierClick(object sender, RoutedEventArgs e)
-        {
-            ComboBoxItem typeItem = (ComboBoxItem)Menu.SelectedItem;
-            if (typeItem != null)
-            {
-                if (typeItem.Content != null)
-                {
-                    string selected = typeItem.Content.ToString();
-                    if (ListViewPanier.Items != null) ListViewPanier.Items.Add(selected);
-                }
-            }
-        }
-
-        private async void ValiderClick(object sender, RoutedEventArgs e)
+        private async void InitalizeCombobox()
         {
             _userConnected = ((App)(Application.Current)).UserConnected;
             MessageDialog msgDialog = new MessageDialog("Bienvenue " + _userConnected.Pseudo, "Attention");
             await msgDialog.ShowAsync();
+            ListRestaurants = new List<Restaurants>();
+            ListRestaurants = await _service.Restaurants.GetRestaurants();
+        }
+
+        private async void AjouterPanierClick(object sender, RoutedEventArgs e)
+        {
+            if (ListViewPanier.Items != null) ListViewPanier.Items.Add(_menuSelected);
+        }
+
+        private async void ValiderClick(object sender, RoutedEventArgs e)
+        {
         }
 
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
             _userConnected = ((App)(Application.Current)).UserConnected;
             username.Text = _userConnected.Pseudo;
+        }
+
+        private async void RestaurantBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ListMenus = new List<Menus>();
+            RestaurantEnCours = RestaurantBox.SelectedValue.ToString();
+            _menuResto = await _service.MenusRestaurant.GetMenuRestaurant(int.Parse(RestaurantEnCours));
+            ListMenus = await _service.Menus.GetMenus();
+            foreach (Menus menu in ListMenus)
+            {
+                if (menu.IdMenus == _menuResto.IdMenu)
+                {
+                    _menuSelected = menu;
+                }
+            }
+            MenuResto.Text = _menuSelected.NomMenus;
         }
     }
 }
